284cf5ccfb8110f84c60d619aa2cb869e6655f3b
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-03 16:42:50 -0300

    ContatoController and Create endpoint implemented

---
+++ moduloAPI/Controllers/ContatoController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using moduloAPI.Repositories;
+using moduloAPI.Models;
+
+namespace moduloAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaRepository _dbContext;
+
+        public ContatoController(AgendaRepository dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost("CriarContato")]
+        public IActionResult Create(Contato contato)
+        {
+            _dbContext.Add(contato);
+            _dbContext.SaveChanges();
+            
+            return Ok(contato);
+        }
+    }
+}
