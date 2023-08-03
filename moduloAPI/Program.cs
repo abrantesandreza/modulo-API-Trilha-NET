6a4841e4c39096e633ecd8e0adc69dff5af69dd0
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-02 23:33:01 -0300

    ConnectionString configured

--- moduloAPI/Program.cs
+++ moduloAPI/Program.cs
@@ -1,6 +1,11 @@
+using moduloAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaRepository>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProjetoModuloAPIAgenda")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
